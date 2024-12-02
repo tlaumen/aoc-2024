b7d13a53aac82bf48caabdc267883fa7a39ba52e
Tom Laumen <tom_laumen@hotmail.com>
2024-12-02 17:59:41 +0100

    part 2

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 
 fn main() {
-    day_1_1();
+    day_1();
 }
 
-fn day_1_1() {
+fn day_1() {
     let rdr = csv::Reader::from_path("day1_1.csv");
     let reader = match rdr {
         Ok(reader) => reader,
@@ -25,7 +25,6 @@ fn day_1_1() {
         for l in line{
             let l = l.split("   ");
             for part in l {
-                println!("{}", part);
                 if i == 0 {
                     v1.push(part.parse().expect("Not a integer"));
                 } else {
@@ -36,6 +35,8 @@ fn day_1_1() {
 
         };
     };
+
+    // Part 1: sum all differences with lists sorted
     v1.sort();
     v2.sort();
     let mut s: i32 = 0;
@@ -44,4 +45,13 @@ fn day_1_1() {
         s += diff.abs();
     };
     println!("{}", s);
+
+    // Part 2: find number of occurances of elements of v1 in v2 and sum product of number of occurences and number itself
+    let mut s2 = 0;
+    for n1 in v1.iter(){
+        // let n2 = n1 as usize;
+        let n_occurences =  v2.iter().filter(|&x| x == n1).count() as i32;
+        s2 += n1 * n_occurences;
+    }
+    println!("{}", s2);
 }
